d5e168bacdf25dd855f87d3f22b18130f2d81a40
lenzyh <139096478+lenzyh@users.noreply.github.com>
2023-11-07 18:43:39 +0800

    Update dbi_news.py

--- dbi_news.py
+++ dbi_news.py
@@ -335,7 +335,7 @@ if page == "Sport's Trend":
     st.pyplot(plt)  # Display the Matplotlib figure using st.pyplot
     st.subheader("Topic's Modelling")
     # Create a BERTopic model
-    topic_model = BERTopic(embedding_model=sentence_model, calculate_probabilities=True)
+    topic_model = BERTopic()
     
     # Fit the BERTopic model to the preprocessed 'processed_title' data
     topics, probabilities = topic_model.fit_transform(processed['processed_title'])
