941c606bd19ef353ae73c8539dc8b9c336784784
Teezq <87978642+Teezq@users.noreply.github.com>
2023-11-14 15:18:31 +0800

    Update dbi_news.py

--- dbi_news.py
+++ dbi_news.py
@@ -74,7 +74,7 @@ if page == "Top Online Casinos":
     st.image("https://lenzyh.github.io/testing_app/data/03_TopCasino_DescriptionBarChart.png", use_column_width=True)
 
     st.header('Topic Modelling')
-    st.image("https://lenzyh.github.io/testing_app/data/1_TopCasino_TopicWordClouds.png", width=1000,height=800)    
+    st.image("https://lenzyh.github.io/testing_app/data/1_TopCasino_TopicWordClouds.png", use_column_width=True, use_column_height=True)    
     components.iframe("https://lenzyh.github.io/testing_app/data/2_TopCasino_TopicWordScore.html",width=1000,height=800)
     components.iframe("https://lenzyh.github.io/testing_app/data/3_TopCasino_TopicGrouping.html",width=1000,height=800)
     components.iframe("https://lenzyh.github.io/testing_app/data/4_TopCasino_InterDisMap.html",width=1000,height=800)
