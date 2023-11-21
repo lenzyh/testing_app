776d1ff9a23eed20b1c2907a47bd4e35e1b8cbb7
lenzyh <139096478+lenzyh@users.noreply.github.com>
2023-11-21 10:59:28 +0800

    Update dbi_news.py

--- dbi_news.py
+++ dbi_news.py
@@ -659,9 +659,9 @@ if page == "NBA Match":
      roster)
     df_lineup = df_team[df_team['GROUP_NAME'] == roster]
     df_important = df_lineup[['MIN','PLUS_MINUS','PTS','AST','REB','FG_PCT', 'FG3_PCT']]
-    df_important=df_important.sort_values(by=['MIN','PLUS_MINUS'],ascending=[FALSE,FALSE]
+    df_important=df_important.sort_values(by=['MIN','PLUS_MINUS'],ascending=[FALSE,FALSE])
     #st.markdown(df_important.style.hide(axis="index").to_html(escape=False), unsafe_allow_html=True)
-    col1, col2, col3, col4,col5,col6,col7 = st.columns(4)
+    col1, col2, col3, col4,col5,col6,col7 = st.columns(7)
     with col1: 
         fig_min = px.histogram(df_team, x="MIN")
         fig_min.add_vline(x=df_important['MIN'].values[0],line_color='red')
