9c1eb132b53bd17d2e53434f8a4d993269763bdc
lenzyh <139096478+lenzyh@users.noreply.github.com>
2023-11-22 19:40:42 +0800

    Update dbi_news.py

--- dbi_news.py
+++ dbi_news.py
@@ -712,39 +712,20 @@ if page == "NBA Match":
         st.plotly_chart(fig_7, use_container_width=True)
     st.subheader('NBA Player Shot Chart')
     nba_shot_data_regular=nba_shotchart
-    def draw_court(ax=None, color="blue", lw=1, shotzone=False, outer_lines=False):
-        """Returns an axes with a basketball court drawn onto to it.
-        This function draws a court based on the x and y-axis values that the NBA
-        stats API provides for the shot chart data.  For example the center of the
-        hoop is located at the (0,0) coordinate.  Twenty-two feet from the left of
-        the center of the hoop in is represented by the (-220,0) coordinates.
-        So one foot equals +/-10 units on the x and y-axis.
-        Parameters
-        ----------
-        ax : Axes, optional
-            The Axes object to plot the court onto.
-        color : matplotlib color, optional
-            The color of the court lines.
-        lw : float, optional
-            The linewidth the of the court lines.
-        outer_lines : boolean, optional
-            If `True` it draws the out of bound lines in same style as the rest of
-            the court.
-        Returns
-        -------
-        ax : Axes
-            The Axes object with the court on it.
-        """
+    def draw_court(ax=None, color='blue', lw=2, outer_lines=False):
+        # If an axes object isn't provided to plot onto, just get current one
         if ax is None:
             ax = plt.gca()
     
         # Create the various parts of an NBA basketball court
     
         # Create the basketball hoop
+        # Diameter of a hoop is 18" so it has a radius of 9", which is a value
+        # 7.5 in our coordinate system
         hoop = Circle((0, 0), radius=7.5, linewidth=lw, color=color, fill=False)
     
         # Create backboard
-        backboard = Rectangle((-30, -12.5), 60, 0, linewidth=lw, color=color)
+        backboard = Rectangle((-30, -7.5), 60, -1, linewidth=lw, color=color)
     
         # The paint
         # Create the outer box 0f the paint, width=16ft, height=19ft
@@ -765,12 +746,13 @@ if page == "NBA Match":
                          color=color)
     
         # Three point line
-        # Create the right side 3pt lines, it's 14ft long before it arcs
+        # Create the side 3pt lines, they are 14ft long before they begin to arc
         corner_three_a = Rectangle((-220, -47.5), 0, 140, linewidth=lw,
                                    color=color)
-        # Create the right side 3pt lines, it's 14ft long before it arcs
         corner_three_b = Rectangle((220, -47.5), 0, 140, linewidth=lw, color=color)
         # 3pt arc - center of arc will be the hoop, arc is 23'9" away from hoop
+        # I just played around with the theta values until they lined up with the 
+        # threes
         three_arc = Arc((0, 0), 475, 475, theta1=22, theta2=158, linewidth=lw,
                         color=color)
     
@@ -779,43 +761,13 @@ if page == "NBA Match":
                                linewidth=lw, color=color)
         center_inner_arc = Arc((0, 422.5), 40, 40, theta1=180, theta2=0,
                                linewidth=lw, color=color)
-        
-        # Draw shotzone Lines
-        # Based on Advanced Zone Mode
-        if (shotzone == True):
-            inner_circle = Circle((0, 0), radius=80, linewidth=lw, color='black', fill=False)
-            outer_circle = Circle((0, 0), radius=160, linewidth=lw, color='black', fill=False)
-            corner_three_a_x =  Rectangle((-250, 92.5), 30, 0, linewidth=lw, color=color)
-            corner_three_b_x = Rectangle((220, 92.5), 30, 0, linewidth=lw, color=color)
-            
-            # 60 degrees
-            inner_line_1 = Rectangle((40, 69.28), 80, 0, 60, linewidth=lw, color=color)
-            # 120 degrees
-            inner_line_2 = Rectangle((-40, 69.28), 80, 0, 120, linewidth=lw, color=color)
-            
-            # Assume x distance is also 40 for the endpoint
-            inner_line_3 = Rectangle((53.20, 150.89), 290, 0, 70.53, linewidth=lw, color=color)
-            inner_line_4 = Rectangle((-53.20, 150.89), 290, 0, 109.47, linewidth=lw, color=color)
-            
-            # Assume y distance is also 92.5 for the endpoint
-            inner_line_5 = Rectangle((130.54, 92.5), 80, 0, 35.32, linewidth=lw, color=color)
-            inner_line_6 = Rectangle((-130.54, 92.5), 80, 0, 144.68, linewidth=lw, color=color)
-            
-            
-            # List of the court elements to be plotted onto the axes
-            court_elements = [hoop, backboard, outer_box, inner_box, top_free_throw,
-                              bottom_free_throw, restricted, corner_three_a,
-                              corner_three_b, three_arc, center_outer_arc,
-                              center_inner_arc, inner_circle, outer_circle,
-                              corner_three_a_x, corner_three_b_x,
-                              inner_line_1, inner_line_2, inner_line_3, inner_line_4, inner_line_5, inner_line_6]
-        else:
-            # List of the court elements to be plotted onto the axes
-            court_elements = [hoop, backboard, outer_box, inner_box, top_free_throw,
-                              bottom_free_throw, restricted, corner_three_a,
-                              corner_three_b, three_arc, center_outer_arc,
-                              center_inner_arc]
-        
+    
+        # List of the court elements to be plotted onto the axes
+        court_elements = [hoop, backboard, outer_box, inner_box, top_free_throw,
+                          bottom_free_throw, restricted, corner_three_a,
+                          corner_three_b, three_arc, center_outer_arc,
+                          center_inner_arc]
+    
         if outer_lines:
             # Draw the half court line, baseline and side out bound lines
             outer_lines = Rectangle((-250, -47.5), 500, 470, linewidth=lw,
@@ -825,10 +777,8 @@ if page == "NBA Match":
         # Add the court elements onto the axes
         for element in court_elements:
             ax.add_patch(element)
-            
     
         return ax
-    plt.rcParams['figure.figsize'] = (12, 11)
     player=st.selectbox('Select', nba_shot_data_regular['PLAYER_NAME'].unique())
     player_shotchart_df=nba_shot_data_regular[(nba_shot_data_regular['PLAYER_NAME']==player)]
     league=nba_shot_data_regular[['GRID_TYPE','SHOT_ZONE_BASIC','SHOT_ZONE_AREA','SHOT_ZONE_RANGE','SHOT_ATTEMPTED_FLAG','SHOT_MADE_FLAG']]
